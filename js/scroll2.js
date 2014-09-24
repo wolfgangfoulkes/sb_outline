693b6534c738330ec6c2fb1973bcbc4c5b40a9ce
wolfgangfoulkes <wolfgangfoulkes@alum.calarts.edu>
2014-09-23 17:09:48 -0700

    …

--- js/scroll2.js
+++ js/scroll2.js
@@ -17,42 +17,42 @@ jQuery(document).ready(function($) {
             
             console.log("window.x", w.x);
             console.log("window.y", w.y);
-        }
+        },
         
-    };
+        scrollFix: function() {
+            scr_t = $(this).scrollTop();
+            scr_b = scr_t + w.y;
+
+            $(".content-item").each(function(index, element) {
+                var top = $(element).offset().top;
+                var height = $(element).outerHeight(false);
+                var bottom = top + height;
+                var $frame = $(element).find(".content-item-frame");
+                var step = 0;
+                if ((scr_b >= top) && (scr_t < bottom))
+                {
+                    if (scr_b < bottom)
+                    {
+                        step = 1;
+                    }
+                    else
+                    {
+                        step = 2;
+                    }
+                }
+                $frame.toggleClass("abs-top", (step == 0));
+                $frame.toggleClass("fixed", (step == 1));
+                $frame.toggleClass("abs-bottom", (step == 2));
+                console.log("index", index, "scr_t", scr_t, "scr_b", scr_b, "top", top, "bottom", bottom, "step", step);
+                });
+            }
+        };
     
        /*****RUNTIME*****/
     
     $(window).resize(callbacks.windowSize);
-    $(window).scroll(function(){
-        scr_t = $(this).scrollTop();
-        scr_b = scr_t + w.y;
-
-        $(".content-item").each(function(index, element) {
-            var top = $(element).offset().top;
-            var height = $(element).outerHeight(false);
-            var bottom = top + height;
-            var $frame = $(element).find(".content-item-frame");
-            var step = 0;
-            if ((scr_b >= top) && (scr_t < bottom))
-            {
-                if (scr_b < bottom)
-                {
-                    step = 1;
-                }
-                else
-                {
-                    step = 2;
-                }
-            }
-            $frame.toggleClass("abs-top", (step == 0));
-            $frame.toggleClass("fixed", (step == 1));
-            $frame.toggleClass("abs-bottom", (step == 2));
-            console.log("index", index, "scr_t", scr_t, "scr_b", scr_b, "top", top, "bottom", bottom, "step", step);
-            
-        });
-        
-    });
+    $(window).resize(callbacks.scrollFix);
+    $(window).scroll(callbacks.scrollFix);
     
 });
 
