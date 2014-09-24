c8b612c0e47151654a9a63801f9869235db781ae
wolfgangfoulkes <wolfgangfoulkes@alum.calarts.edu>
2014-09-23 23:18:53 -0700

    implemented a more modular method of doing this

--- js/scroll2.js
+++ js/scroll2.js
@@ -22,29 +22,41 @@ jQuery(document).ready(function($) {
         scrollFix: function() {
             scr_t = $(this).scrollTop();
             scr_b = scr_t + w.y;
+            $(".scroll-outer").each(function(index, element){
+                var $outer = $(element);
+                
+                //get height of outer element just once
+                var top_o = $outer.offset().top;
+                var height_o = $outer.outerHeight(false); //no margins
+                var bottom_o = top_o + height_o;
+                
+                //iterate over all .scroll objects
+                $(".scroll-inner").each(function(index, element) {
+                    var $inner = $(element);
 
-            $(".content-item").each(function(index, element) {
-                var top = $(element).offset().top;
-                var height = $(element).outerHeight(false);
-                var bottom = top + height;
-                var state = "off"; //same as defining an else
-                if ((scr_b >= top) && (scr_t < bottom)) //thing is onscreen
-                {
-                    if (scr_t >= top)
+                    var top_i = $inner.offset().top;
+                    var height_i = $inner.outerHeight(true);
+                    //if "true" it keeps reading that "top" position. a more semantic solution would be "transform"
+                    var bottom_i = top_o + height_i;
+                    
+                    var state = "below-scroll"; //same as defining an else
+                    if ((scr_b >= top_o) && (scr_t < bottom_o)) //thing is onscreen
+                    {
+                        if (scr_b >= bottom_i)
                         {
-                            if (scr_b >= bottom) {state = "m_bottom";} //margin for margin-is
-                            else {state = "fullscreen";}
+                            if (scr_b >= bottom_o) {state = "above-scroll";}
+                            else {state = "scroll"}
                         }
-                    else {state = "m_top";}
-                }
-                $(element).toggleClass("off", (state == "off") );
-                $(element).toggleClass("m_top", (state == "m_top") );
-                $(element).toggleClass("fullscreen", (state == "fullscreen") );
-                $(element).toggleClass("m_bottom", (state == "m_bottom") );
-                console.log("index", index, "scr_t", scr_t, "scr_b", scr_b, "state", state);
+                    }
+                    $inner.toggleClass("below-scroll", (state == "below-scroll") );
+                    $inner.toggleClass("scroll", (state == "scroll") );
+                    $inner.toggleClass("above-scroll", (state == "above-scroll") );
+                    console.log("index", index, "bottom_o", bottom_o, "top_o:", top_o, "bottom_i", bottom_i, "top_i:", top_i, "scr_t", scr_t, "scr_b", scr_b, "state", state);
                 });
-            }
-        };
+            });
+        }
+    };
+    
     
        /*****RUNTIME*****/
     
